88389f447156e0dfc9868d67b6cd384ceff88597
Dmitry Gavrilenko <gavrilaz0076@gmail.com>
2023-05-04 15:06:36 +0300

    Final

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def item_router():
+    return Item("роутер", 1000, 10)
+
+def test_item_init(item_router):
+    assert item_router.name == "роутер"
+
+def test_calculate_total_price(item_router):
+    assert item_router.calculate_total_price() == 10000
+
+def test_apply_discount(item_router):
+    Item.pay_rate = 0.5
+    item_router.apply_discount()
+    assert item_router.price == 500
